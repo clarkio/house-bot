fa524aa1435b8c8469f5f6648caee0a3f5ea484b
clarkio <clarkio@users.noreply.github.com>
2018-02-15 15:57:00 -0500

    change it up

--- messages/index.js
+++ messages/index.js
@@ -1,72 +1,64 @@
-/*-----------------------------------------------------------------------------
-This template demonstrates how to use an IntentDialog with a LuisRecognizer to add 
-natural language support to a bot. 
-For a complete walkthrough of creating this type of bot see the article at
-https://aka.ms/abs-node-luis
------------------------------------------------------------------------------*/
-"use strict";
-var builder = require("botbuilder");
-var botbuilder_azure = require("botbuilder-azure");
-var path = require('path');
+var builder = require('botbuilder');
+require('dotenv').config();
+var botbuilder_azure = require('botbuilder-azure');
 
-var useEmulator = (process.env.NODE_ENV == 'development');
-
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
+var connector = new botbuilder_azure.BotServiceConnector({
+  appId: process.env['MicrosoftAppId'],
+  appPassword: process.env['MicrosoftAppPassword'],
+  openIdMetadata: process.env['BotOpenIdMetadata']
 });
-
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 var bot = new builder.UniversalBot(connector);
-bot.localePath(path.join(__dirname, './locale'));
-bot.set('storage', tableStorage);
 
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+let luisModelUrl = `https://${process.env['LuisAPIHostName']}/luis/v2.0/apps/${
+  process.env['LuisAppId']
+}?subscription-key=${process.env['LuisAPIKey']}`;
 
 // Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
-})
-.matches('Help', (session) => {
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
-})
-.matches('Cancel', (session) => {
-    session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
-})
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-});
+const recognizer = new builder.LuisRecognizer(luisModelUrl);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] })
+  .matches('Greeting', session => {
+    session.send('Sup, yo!');
+  })
+  .matches('Thank You', session => {
+    session.send('No problem! Glad I could help.');
+  })
+  .matches('Help', session => {
+    session.send('I can control the lights in your house. You can say things like, "Turn the kitchen lights on".');
+  })
+  .matches('Cancel', session => {
+    session.send('OK. Canceled.');
+    session.endDialog();
+  })
+  .matches('Control Lights', (session, args) => {
+    session.send('OK! One sec...');
 
-bot.dialog('/', intents);    
+    var location = builder.EntityRecognizer.findEntity(args.entities, 'Location');
 
-if (useEmulator) {
-    var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(3978, function() {
-        console.log('test bot endpont at http://localhost:3978/api/messages');
-    });
-    server.post('/api/messages', connector.listen());    
-} else {
-    module.exports = connector.listen();
+    var lightState = builder.EntityRecognizer.findEntity(args.entities, 'Light State');
+
+    // got both location and light state, move on to the next step
+    if (location && lightState) {
+      // we call LIFX
+      controlLights(session, location.entity, lightState.entity);
+    }
+
+    // got a location, but no light state
+    if (!location || !lightState) {
+      session.send(
+        `I need to know which light and if you want it on or off. You can say things like, "Turn on/off the light".`
+      );
+    }
+  })
+  .onDefault(session => {
+    session.send("Sorry, I did not understand '%s'.", session.message.text);
+  });
+
+bot.dialog('/', intents);
+
+function controlLights(session, location, lightState) {
+  session.send(`${location} lights are ${lightState}`);
+  session.endDialog();
 }
 
+module.exports = connector.listen();
