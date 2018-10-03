1be31ed7d850ac8673a8c99cd5b792c2b9cf841b
clarkio <clarkio@users.noreply.github.com>
2018-10-03 15:01:52 -0400

    WIP: moar refactoring for constants

--- messages/constants.js
+++ messages/constants.js
@@ -13,27 +13,51 @@ module.exports = {
   ENTITY_COLOR_NAME: 'color',
   ENTITY_EFFECT_NAME: 'effect',
   ENTITY_STATE_NAME: 'state',
-  MESSAGE_COMMAND_NOT_UNDERSTOOD:
+  MESSAGE_LIGHT_COMMAND_NOT_UNDERSTOOD:
     'I did not understand that light command. Please double check the available commands and retry.',
   PULSE_EFFECT_OPTIONS_COP_MODE: {
     color: 'blue',
     from_color: 'red',
-    period,
-    cycles,
     power_on: true
   },
   PULSE_EFFECT_OPTIONS_NEW_FOLLOWER: {
     color: 'purple',
     from_color: 'white',
-    period,
-    cycles,
     power_on: true
   },
   PULSE_EFFECT_OPTIONS_NEW_SUBSCRIBER: {
     color: 'green',
     from_color: 'purple',
-    period,
-    cycles,
     power_on: true
-  }
+  },
+  EFFECT_COP_MODE: 'cop mode',
+  EFFECT_NEW_FOLLOWER: 'new follower',
+  EFFECT_NEW_SUBSCRIBER: 'new subscriber',
+  LOG_FULL_MESSAGE_RECEIVED: logFullMessageReceivedTag,
+  LOG_UNSUPPORTED_EFFECT: logUnsupportedEffect,
+
+
+
+
+
+  LOG_EFFECT_TRIGGERED: logEffectTriggered
 };
+
+function logEffectTriggered(effect) {
+  return `The effect triggered was: ${effect}`;
+}
+
+
+
+
+
+
+
+
+function logFullMessageReceivedTag(message) {
+  return `Full message received: ${message}`;
+}
+
+function logUnsupportedEffect(effect) {
+  return `Received an unsupported effect: ${effect}`;
+}
